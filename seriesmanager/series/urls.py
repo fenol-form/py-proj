0c12b0ad1fcbb49e486d6267edca4d9763e28050
ntyazh <ntyazh@mail.ru>
2022-12-05 13:50:09 +0300

    Search page

--- seriesmanager/series/urls.py
+++ seriesmanager/series/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("login/", Login.as_view(), name="login"),
     path("registration", Registration.as_view(), name="registration"),
-    path("logout", sign_out, name="logout")
+    path("logout", sign_out, name="logout"),
+    path("search", SearchPage.as_view(), name="search"),
+    path("result", ResultPage.as_view(), name="result")
 ]
